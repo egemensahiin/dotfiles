e2b663731f4087dc4b0b8955de4e1efe989c453e
Egemen Sahin <egemensahiin@github.com>
2020-08-04 17:30:30 +0300

    Minor changes.

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 udiskie &
-feh --bg-scale ~/Pictures/wallpapers/wallp3.jpg &
+feh --bg-scale ~/Pictures/wallpapers/wallp1.png &
 nm-applet &
 blueman-tray &
 xidlehook --not-when-fullscreen --not-when-audio --timer 300 "i3lock -e -i /home/egemen/Pictures/wallpapers/wallp5.png" normal &
