edc9f829209681f94512a2c2dc81691a49fd73d4
Egemen Sahin <egemensahiin@github.com>
2020-08-07 18:46:38 +0300

    Added warning when took screenshot.

--- .scripts/screenshot.sh
+++ .scripts/screenshot.sh
@@ -14,10 +14,14 @@ NUM=1
 while [[ -f ${FILE} ]]
 do
 	FILE="${FILE}(${NUM})"
+	NUM=$(( NUM + 1 ))
 done
 
 FILE_NAME="${FILE}.png"
 
 maim | tee ${FILE_NAME} | xclip -selection clipboard -t image/png
+
+i3-nagbar -t warning -f "Inconsolata Regular 9" -m "Screenshot saved as $(basename ${FILE_NAME})." -B 'Delete' "rm ${FILE_NAME}" & sleep 3s
+pkill i3-nagbar
 
 exit 0
