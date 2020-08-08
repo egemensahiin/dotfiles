b73fb03d8fcabeb4e2ba7b70c09fd4eb966ea71c
Egemen Sahin <egemensahiin@github.com>
2020-08-09 01:44:52 +0300

    Minor change.

--- .scripts/screenshot.sh
+++ .scripts/screenshot.sh
@@ -2,26 +2,25 @@
 
 SAVE="/home/egemen/Pictures/Screenshots"
 
-if [[ ! -d $SAVE ]]
-then
-	mkdir "$SAVE"
-fi
+[ ! -d $SAVE ] && mkdir "$SAVE"
 
 FILE="${SAVE}/Screenshot$(date +%Y%m%d%H%M%S)"
 
-NUM=1
-
-while [[ -f ${FILE} ]]
-do
-	FILE="${FILE}(${NUM})"
-	NUM=$(( NUM + 1 ))
-done
-
 FILE_NAME="${FILE}.png"
+
+# -- CHECK HERE: It seemed very simple but it isn't work, idk why.
+# NUM=1
+# 
+# while [[ -f "${FILE_NAME}" ]]
+# do
+# 	FILE_NAME="${FILE}(${NUM}).png"
+# 	NUM=$(( NUM + 1 ))
+# done
 
 maim | tee ${FILE_NAME} | xclip -selection clipboard -t image/png
 
-i3-nagbar -t warning -f "Inconsolata Regular 9" -m "Screenshot saved as $(basename ${FILE_NAME})." -B 'Delete' "rm ${FILE_NAME}" & sleep 3s
+i3-nagbar -t warning -f "Inconsolata Regular 9" -m "Screenshot saved as $(basename ${FILE_NAME})." -B 'Delete' "rm ${FILE_NAME}" 
+sleep 3s
 pkill i3-nagbar
 
 exit 0
