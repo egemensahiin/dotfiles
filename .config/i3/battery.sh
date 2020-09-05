3776986886a45567a3f5a2736ef65387ad86bbd4
egemensahiin <egemensahin95@gmail.com>
2020-09-06 01:21:36 +0300

    Minor changes on battery script for i3blocks.

--- .config/i3/battery.sh
+++ .config/i3/battery.sh
@@ -2,7 +2,7 @@
 
 LEVEL="$(acpi -b | grep -v "unavailable" | awk -F "," '{print $2}' | cut -d "%" -f1 | cut -d " " -f2)"
 STAT="$(acpitool | head -n 1 | awk '{print $4}')"
-if [[ $STAT == "Discharging," ]]
+if [[ $STAT == "Discharging," || $STAT == "Full," ]]
 then
 		if [[ $LEVEL -gt 95 ]]
 		then
