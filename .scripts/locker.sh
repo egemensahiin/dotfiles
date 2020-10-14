96dbb59e7d75e219c9d5bc74a461f2cce45aa5ba
egemensahiin <egemensahin95@gmail.com>
2020-10-15 01:14:47 +0300

    Changed locker to i3lock-colors and configured a locker.

---
+++ .scripts/locker.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# a really nice looking setup to lock screen
+
+PIC=/tmp/i3lock.png
+
+BLUR="10x8"
+
+maim | tee /tmp/i3lock.png
+
+convert $PIC -blur $BLUR $PIC
+i3lock 	-e -i $PIC \
+		--clock \
+		--datestr="%A, %d %m %Y" \
+		--linecolor=#282828 \
+		--verifcolor=#ebdbb2 \
+		--wrongcolor=#ebdbb2 \
+		--timecolor=#ebdbb2 \
+		--datecolor=#ebdbb2 \
+		--greetercolor=#ebdbb2 \
+		--ringcolor=#98971a \
+		--insidecolor=282828d9 \
+		--keyhlcolor=#b8bb26 \
+		--bshlcolor=#cc241d \
+		--ringvercolor=#458588 \
+		--insidevercolor=83a598d9 \
+		--ringwrongcolor=#cc241d \
+		--insidewrongcolor=fb4934d9 \
+		--time-font=Inconsolata \
+		--date-font=Inconsolata \
+		--layout-font=Inconsolata \
+		--verif-font=Inconsolata \
+		--wrong-font=Inconsolata \
+		--greeter-font=Inconsolata \
+		--noinputtext="No Input" \
+		--locktext="Locking" \
+		--veriftext="Verifying" \
+		--wrongtext="Wrong!" \
+		--lockfailedtext="Lock Failed!" \
+		--greetertext="Locked" \
+		--greeterpos="960:580" \
+		--datepos="960:550" \
+		--timepos="960:530" \
+		--radius=100 \
+		--ring-width 3.0 \
+		--indicator
+rm $PIC
